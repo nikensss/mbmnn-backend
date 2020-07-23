b509a3f96cb9ce1552d538e65a71a907412cc703
nikens-mbp16 <rikitzzz@gmail.com>
2020-07-23 20:55:53 +0200

    using local mongodb instance

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,13 +1,10 @@
 import mongoose from 'mongoose';
 
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0-0qo8b.mongodb.net/mbmnn?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  )
+  .connect('mongodb://localhost/mbmnn', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(data => {
     console.log('Connection to DB successful!');
   })
