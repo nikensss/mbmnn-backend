c610bce2c1e670a8586ecb5ba892d4625c703a90
nikens-mbp16 <rikitzzz@gmail.com>
2020-10-27 10:09:38 +0100

    prettier

--- src/models/Project.js
+++ src/models/Project.js
@@ -2,11 +2,15 @@ import { Schema } from 'mongoose';
 import mongoose from '../database/mongodb';
 
 const Project = new Schema({
-  title: { type: String, required: [true, 'title is required'], index: { unique: true } },
+  title: {
+    type: String,
+    required: [true, 'title is required'],
+    index: { unique: true }
+  },
   description: { type: String, required: [true, 'description is required'] },
   texts: { type: [String] },
   mainImage: { data: Buffer, contentType: String },
-  images: [{data: Buffer, contentType: String}]
+  images: [{ data: Buffer, contentType: String }]
 });
 
 export default mongoose.model('Project', Project);
